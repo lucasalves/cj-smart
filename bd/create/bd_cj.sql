9bb5f39014956306deeca029a72ecc96868e5ff1
Fernando Italo Siebra Siqueira <fernandosiebra@gmail.com>
2013-07-01 20:26:20 -0300

    create table Educador-Materia

--- bd/create/bd_cj.sql
+++ bd/create/bd_cj.sql
@@ -3,7 +3,7 @@
 -- Server version:               5.5.28 - MySQL Community Server (GPL)
 -- Server OS:                    Win64
 -- HeidiSQL version:             7.0.0.4234
--- Date/time:                    2013-05-07 21:26:46
+-- Date/time:                    2013-07-01 20:20:01
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -11,35 +11,106 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Dumping structure for table bd_cj.tb_curso
-CREATE TABLE IF NOT EXISTS `tb_curso` (
-  `curso_id` int(10) NOT NULL AUTO_INCREMENT,
-  `curso_nome` varchar(50) DEFAULT NULL,
-  `curso_descricao` varchar(50) DEFAULT NULL,
-  `curso_duracao` int(11) DEFAULT NULL,
-  PRIMARY KEY (`curso_id`)
+-- Dumping structure for table cjsmart.aluno
+CREATE TABLE IF NOT EXISTS `aluno` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) DEFAULT NULL,
+  `rg` varchar(20) DEFAULT NULL,
+  `cpf` varchar(20) DEFAULT NULL,
+  `logradouro` varchar(100) DEFAULT NULL,
+  `cep` varchar(80) DEFAULT NULL,
+  `responsavel` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
 
--- Dumping structure for table bd_cj.tb_materia
-CREATE TABLE IF NOT EXISTS `tb_materia` (
-  `materia_id` int(10) NOT NULL AUTO_INCREMENT,
-  `materia_nome` varchar(100) NOT NULL,
-  `materia_codigo` varchar(50) NOT NULL,
-  PRIMARY KEY (`materia_id`)
+-- Dumping structure for table cjsmart.curso
+CREATE TABLE IF NOT EXISTS `curso` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  `descricao` text,
+  `duracao` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table cjsmart.curso_materia
+CREATE TABLE IF NOT EXISTS `curso_materia` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `materia_id` int(11) NOT NULL,
+  `curso_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
 
--- Dumping structure for table bd_cj.tb_materia_curso
-CREATE TABLE IF NOT EXISTS `tb_materia_curso` (
-  `materia_curso_id` int(10) NOT NULL AUTO_INCREMENT,
-  `materia_id` int(11) NOT NULL DEFAULT '0',
-  `curso_id` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`materia_curso_id`)
+-- Dumping structure for table cjsmart.educador
+CREATE TABLE IF NOT EXISTS `educador` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `curriculo` text NOT NULL,
+  `rg` varchar(12) NOT NULL,
+  `endereco_id` int(11) unsigned zerofill NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table cjsmart.educador_materia
+CREATE TABLE IF NOT EXISTS `educador_materia` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `educador_id` int(10) NOT NULL,
+  `materia_id` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table cjsmart.endereco
+CREATE TABLE IF NOT EXISTS `endereco` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `logradouro` varchar(200) NOT NULL,
+  `complemento` varchar(200) NOT NULL,
+  `bairro` varchar(100) NOT NULL,
+  `cidade` varchar(100) NOT NULL,
+  `estado` varchar(2) NOT NULL,
+  `cep` varchar(9) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table cjsmart.materia
+CREATE TABLE IF NOT EXISTS `materia` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  `codigo` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table cjsmart.responsavel
+CREATE TABLE IF NOT EXISTS `responsavel` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telefone` varchar(100) NOT NULL,
+  `endereco_id` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
