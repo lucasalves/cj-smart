86243cf8301f6cd6d6cb148deb3dc422e5548a05
Lucas Alves <lucas_alvessp@icloud.com>
2013-08-04 17:54:49 -0300

    SQL

--- database/sql/cjsmart.sql
+++ database/sql/cjsmart.sql
@@ -1,31 +1,45 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.10.1deb1
+-- version 4.0.4.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: May 20, 2013 at 08:02 PM
--- Server version: 5.5.31
--- PHP Version: 5.3.10-1ubuntu3.6
+-- Máquina: localhost
+-- Data de Criação: 04-Ago-2013 às 20:51
+-- Versão do servidor: 5.6.12
+-- versão do PHP: 5.3.15
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
--- Database: `cjsmart`
+-- Base de Dados: `cjsmart`
 --
+CREATE DATABASE IF NOT EXISTS `cjsmart` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `cjsmart`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `curso`
+-- Estrutura da tabela `aluno`
 --
 
+DROP TABLE IF EXISTS `aluno`;
+CREATE TABLE IF NOT EXISTS `aluno` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) DEFAULT NULL,
+  `rg` varchar(20) DEFAULT NULL,
+  `cpf` varchar(20) DEFAULT NULL,
+  `logradouro` varchar(100) DEFAULT NULL,
+  `cep` varchar(80) DEFAULT NULL,
+  `responsavel` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `curso`
+--
+
+DROP TABLE IF EXISTS `curso`;
 CREATE TABLE IF NOT EXISTS `curso` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(45) DEFAULT NULL,
@@ -33,87 +47,61 @@ CREATE TABLE IF NOT EXISTS `curso` (
   `duracao` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `curso_materia`
+-- Estrutura da tabela `curso_materia`
 --
 
+DROP TABLE IF EXISTS `curso_materia`;
 CREATE TABLE IF NOT EXISTS `curso_materia` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `materia_id` int(11) NOT NULL,
   `curso_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-)ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `materia`
+-- Estrutura da tabela `educador`
 --
 
-CREATE TABLE IF NOT EXISTS `materia` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(45) DEFAULT NULL,
-  `codigo` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2;
-
---
--- Dumping data for table `materia`
---
-
-INSERT INTO `materia` (`id`, `nome`, `codigo`) VALUES
-(1, 'Materia do Cabelo', '01');
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
-CREATE TABLE `alunos` (
-	`id` INT(10) NOT NULL AUTO_INCREMENT,
-	`nome` VARCHAR(50) NULL DEFAULT NULL,
-	`rg` VARCHAR(20) NULL DEFAULT NULL,
-	`cpf` VARCHAR(20) NULL DEFAULT NULL,
-	`logradouro` VARCHAR(100) NULL DEFAULT NULL,
-	`cep` VARCHAR(80) NULL DEFAULT NULL,
-	`responsavel` VARCHAR(100) NULL DEFAULT NULL,
-	PRIMARY KEY (`id`)
-)ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
-
-
--- --------------------------------------------------------
--- Host:                         localhost
--- Server version:               5.5.28 - MySQL Community Server (GPL)
--- Server OS:                    Win64
--- HeidiSQL version:             7.0.0.4234
--- Date/time:                    2013-05-20 22:44:17
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
--- Dumping structure for table cjsmart.educador
+DROP TABLE IF EXISTS `educador`;
 CREATE TABLE IF NOT EXISTS `educador` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `curriculo` text NOT NULL,
-  `endereco_id` int(11) NOT NULL,
+  `rg` varchar(12) NOT NULL,
+  `endereco_id` int(11) unsigned zerofill NOT NULL,
   PRIMARY KEY (`id`)
-)ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+);
 
--- Data exporting was unselected.
+-- --------------------------------------------------------
 
+--
+-- Estrutura da tabela `educador_materia`
+--
 
--- Dumping structure for table cjsmart.endereco
+DROP TABLE IF EXISTS `educador_materia`;
+CREATE TABLE IF NOT EXISTS `educador_materia` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `educador_id` int(10) NOT NULL,
+  `materia_id` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `endereco`
+--
+
+DROP TABLE IF EXISTS `endereco`;
 CREATE TABLE IF NOT EXISTS `endereco` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `logradouro` varchar(200) NOT NULL,
@@ -123,12 +111,68 @@ CREATE TABLE IF NOT EXISTS `endereco` (
   `estado` varchar(2) NOT NULL,
   `cep` varchar(9) NOT NULL,
   PRIMARY KEY (`id`)
-)ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+);
 
--- Data exporting was unselected.
+-- --------------------------------------------------------
 
+--
+-- Estrutura da tabela `grupo_permissoes`
+--
 
--- Dumping structure for table cjsmart.responsavel
+DROP TABLE IF EXISTS `grupo_permissoes`;
+CREATE TABLE IF NOT EXISTS `grupo_permissoes` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `pagina` varchar(45) NOT NULL,
+  `visualizar` tinyint(1) NOT NULL DEFAULT '1',
+  `editar` tinyint(1) NOT NULL DEFAULT '1',
+  `apagar` tinyint(1) NOT NULL DEFAULT '1',
+  `usuario_grupos_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+--
+-- Extraindo dados da tabela `grupo_permissoes`
+--
+
+INSERT INTO `grupo_permissoes` (`id`, `pagina`, `visualizar`, `editar`, `apagar`, `usuario_grupos_id`) VALUES
+(1, 'aluno', 1, 1, 1, 1),
+(2, 'curso', 1, 1, 1, 1),
+(3, 'educador', 1, 1, 1, 1),
+(4, 'home', 1, 1, 1, 1),
+(5, 'materia', 1, 1, 1, 1),
+(19, 'responsavel', 1, 1, 1, 1),
+(20, 'turma', 1, 1, 1, 1),
+(21, 'usuarios', 1, 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `materia`
+--
+
+DROP TABLE IF EXISTS `materia`;
+CREATE TABLE IF NOT EXISTS `materia` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  `codigo` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+);
+
+--
+-- Extraindo dados da tabela `materia`
+--
+
+INSERT INTO `materia` (`id`, `nome`, `codigo`) VALUES
+(1, 'Cabelo', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `responsavel`
+--
+
+DROP TABLE IF EXISTS `responsavel`;
 CREATE TABLE IF NOT EXISTS `responsavel` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
@@ -136,9 +180,65 @@ CREATE TABLE IF NOT EXISTS `responsavel` (
   `telefone` varchar(100) NOT NULL,
   `endereco_id` int(10) NOT NULL,
   PRIMARY KEY (`id`)
-)ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+);
 
--- Data exporting was unselected.
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `turma`
+--
+
+DROP TABLE IF EXISTS `turma`;
+CREATE TABLE IF NOT EXISTS `turma` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(200) DEFAULT NULL,
+  `periodo` enum('Manhã','Tarde','Noite') DEFAULT NULL,
+  `data_criacao` date DEFAULT NULL,
+  `curso_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) DEFAULT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `ultimo_nome` varchar(45) DEFAULT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `senha` varchar(50) DEFAULT NULL,
+  `ativo` tinyint(1) NOT NULL DEFAULT '1',
+  `grupo_usuario_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `email`, `nome`, `ultimo_nome`, `usuario`, `senha`, `ativo`, `grupo_usuario_id`) VALUES
+(2, 'admin@hotmail.com', 'Admin', 'admin', 'admin', '202cb962ac59075b964b07152d234b70', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario_grupo`
+--
+
+DROP TABLE IF EXISTS `usuario_grupo`;
+CREATE TABLE IF NOT EXISTS `usuario_grupo` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+--
+-- Extraindo dados da tabela `usuario_grupo`
+--
+
+INSERT INTO `usuario_grupo` (`id`, `nome`) VALUES
+(1, 'master');
