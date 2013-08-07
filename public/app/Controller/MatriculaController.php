70030542226045edcb3297c4aedf2ca50f647671
bruno <brunoipjg@hotmail.com>
2013-08-06 22:40:13 -0300

    Controller Matricula

--- public/app/Controller/MatriculaController.php
+++ public/app/Controller/MatriculaController.php
@@ -29,15 +29,19 @@ App::uses('AppController', 'Controller');
  * @package       app.Controller
  * @link http://book.cakephp.org/2.0/en/controllers/pages-controller.html
  */
-class AlunoController extends AppController {
+class MatriculaController extends AppController {
 
 /**
  * Controller name
  *
  * @var string
  */
-    public $name = 'Aluno';
+    public $name = 'Matricula';
     public $uses = array();
 
     public $scaffold;
+    
+    public function verificar(){
+        
+    }
 }
