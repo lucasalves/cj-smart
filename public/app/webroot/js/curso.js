a9623ce28920eec5407e9e43ddd4f0a70043621c
bruno <brunoipjg@hotmail.com>
2013-08-20 18:59:51 -0300

    Js Curso

--- public/app/webroot/js/curso.js
+++ public/app/webroot/js/curso.js
@@ -1,105 +1,50 @@
 
 (function($){    
-    var Matricula = function (){
+    var Curso = function (){
         var self = this;
 
-        this.aluno;
-           
-        this.verifica = function(){
-            var self = this;
-            $.ajax({
-                dataType: "json",
-                 url: ajaxurl + "matricula/verifica",
-                 data:{
-                     rg: $("#rg").val()
-                 },
-                 success: function(resp){
-                    if(typeof resp.Aluno !== "undefined"){
-                        self.aluno = resp.Aluno;
-                        self.exibeBotao(1);
-                    }else{
-                        self.exibeBotao(2);
-                    }
-                 }
-            });        
-        };
-         
-        this.exibeBotao = function(status){
-            if(status == 1){
-                $("#MatriculaAlunoId").val(this.aluno.id);
-                var html = 
+        this.curso;
+   
+        this.addMateria = function(){
+            $.get(ajaxurl + 'materia/add', function(response){
+                var html = $(response).find("#corpo").html();
+                App.Modal.add(html, true, function(){
                     
-                "<dt>Nome:</dt>"+
-                "<dd>"+this.aluno.nome+"</dd>"+
-                    
-                "<dt>Rg:</dt>"+
-                "<dd>"+this.aluno.rg+"</dd>"+
-
-                "<dt>CPF:</dt>"+
-                "<dd>"+this.aluno.cpf+"</dd>"+
-
-                "<dt>Logradouro:</dt>"+
-                "<dd>"+this.aluno.logradouro+"</dd>"+
-            
-                "<dt>CEP:</dt>"+
-                "<dd>"+this.aluno.cep+"</dd>"+
-            
-                "<dt>Responsável:</dt>"+
-                "<dd>"+this.aluno.responsavel+"</dd>"+
-                "<br>"+
-                
-                "<input type='submit' class='btn btn-large  btn-success' value='Matricular' />"
-            ;
-                
-            }else if(status == 2){      
-                $.get(ajaxurl + 'aluno/add', {rg: $("#rg").val()}, function(response){
-                    var html = $(response).find("#corpo").html();
-                    App.Modal.add(html, true, function(){
-                        $("#AlunoNome").focus();
                     });                    
-                    $("#botoes").html("");
-                });
-            }
-            
-            $("#botoes").html(html);
-        };
-
-        $('body').on('click', '#UsuarioAddForm #Inserir', function(e){
-            e.preventDefault();
-            
-            var data = {};
-            var vals = $("#UsuarioAddForm input");
-
-            for (var i = 0; i < vals.length; i++) {
-                if(vals[i].name){
-                    data[vals[i].name] = vals[i].value;
-                }
-            };
-
-            self.addAluno(data, function(save){
-
             });
-        });
+        }
+        
+        this.listeners = function (){
+            $('body').on('click', '#MateriaAddForm #Inserir', function(e){
+                e.preventDefault();
+            
+                var data = {};
+                var vals = $("#MateriaAddForm input");
 
-        this.addAluno = function(data, callback){
-            var self = this;
-            var url = ajaxurl + 'aluno/add_ajax';
+                for (var i = 0; i < vals.length; i++) {
+                    if(vals[i].name){
+                        data[vals[i].name] = vals[i].value;
+                    }
+                };
 
-            $.post(url, data, function(result){
-                if(result.status){
-                    self.aluno = result.Aluno;                    
-                    self.exibeBotao(1);
-                    App.Modal.close();             
-                }
-            }, 'json');
+                $.post(ajaxurl + 'materia/add_ajax',data, function(response){
+                    if(response.status){
+                        location.href=location.href;
+                        App.Modal.close();             
+                    }
+                }, 'json');                       
+            });
+                    
+              
         };
+        this.listeners();
     };
 
     $(document) .ready(function(){
-        $("#matricula .find-aluno").on('click', function(e){
+        $("#NovaMateria").on('click', function(e){
             // Instancia de Matricula
-            objMatricula = new Matricula();
-            objMatricula.verifica();
+            objCurso = new Curso();
+            objCurso.addMateria();
         });
     });
 
