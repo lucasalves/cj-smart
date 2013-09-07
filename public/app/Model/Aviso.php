f5e4ebd62401db2d2e0b6476048bf14de6cb5c16
bruno <brunoipjg@hotmail.com>
2013-09-06 21:48:59 -0300

    Nova Entidade Aviso

---
+++ public/app/Model/Aviso.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Application model for Cake.
+ *
+ * This file is application-wide model file. You can put all
+ * application-wide model-related methods here.
+ *
+ * PHP 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       app.Model
+ * @since         CakePHP(tm) v 0.2.9
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+App::uses('AppModel', 'Model');
+
+/**
+ * Application model for Cake.
+ *
+ * Add your application-wide methods in the class below, your models
+ * will inherit them.
+ *
+ * @package       app.Model
+ */
+class Aviso extends AppModel {
+
+    public $useTable = 'aviso';
+    
+    
+    public function addAviso($matricula_id, $aula_id) {
+       
+        $this->set(array(
+            'descricao' => 'Ocorrência Automática - Aluno Faltou mais de 1 vez no período de 5 dias',
+            'status' => 'Enviada',
+            'aula_id' => $aula_id,
+            'matricula_id' => $matricula_id
+        ));
+        
+        return $this->save();
+    }
+
+}
