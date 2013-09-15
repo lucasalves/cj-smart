22f61a64dcf61ce7cb1a5a45fd5962c17c859c9d
Lucas Alves <lucas_alvessp@icloud.com>
2013-09-15 14:36:17 -0300

    Aluno JS (add, view, viewOrAdd)

--- public/app/webroot/js/aluno.js
+++ public/app/webroot/js/aluno.js
@@ -1,5 +1,13 @@
 var Aluno = function(){
 	this.viewOrAdd = function(where, addListerEvent){
-		App.Request.get('aluno/view_or_add', $('.aluno'), addListerEvent);		
+		App.Request.get('aluno/view_or_add', where, $('.aluno'), addListerEvent);		
+	};
+
+	this.add = function(data, addListerEvent){
+		App.Request.post('aluno/add_ajax', data, $('.aluno'), addListerEvent, 'json');
+	};
+
+	this.view = function(id, addListerEvent){
+		App.Request.get('aluno/view/' + id, {}, $('.aluno'), addListerEvent);		
 	};
 };
