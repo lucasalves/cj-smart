88d624e1e1bab7df53b335af9a8aa62c8d971fee
Lucas Alves <lucas_alvessp@icloud.com>
2013-09-15 14:39:41 -0300

    ajustes de melhorias App Request (Ajax)

--- public/app/webroot/js/app/request.js
+++ public/app/webroot/js/app/request.js
@@ -1,18 +1,19 @@
 App.Request = {
 	options: {
 		type: 'get',
-		url: ajaxurl,
+		url: '',
 		dataType: 'html'
 	},
 
-	get: function(url, sel, addListenerEvent, dataType){
+	get: function(url, data, sel, addListenerEvent, dataType){
 		var options 	 = this.options;
 		options.success  = addListenerEvent;
-		options.url  	 += url;
-		options.type  	 += 'get';
+		options.data  	 = data;
+		options.url  	 = ajaxurl + url;
+		options.type  	 = 'get';
 		options.dataType = dataType || 'html';
 		options.beforeSend = function(sel){
-				App.AjaxLoad.show(sel);
+			App.AjaxLoad.show(sel);
 		};
 		options.complete = function(sel){
 			App.AjaxLoad.hide(sel);
@@ -24,12 +25,12 @@ App.Request = {
 	post: function(url, data, sel, addListenerEvent, dataType){
 		var options 	 = this.options;
 		options.success  = addListenerEvent;
-		options.url  	 += url;
-		options.type  	 += 'post';
+		options.url  	 = ajaxurl + url;
+		options.type  	 = 'post';
 		options.dataType = dataType || 'html';
 		options.data 	 = data;
 		options.beforeSend = function(sel){
-				App.AjaxLoad.show(sel);
+			App.AjaxLoad.show(sel);
 		};
 		options.complete = function(sel){
 			App.AjaxLoad.hide(sel);
