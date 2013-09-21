7edfa23887f1f99a841f204ca97506ed60746080
Lucas Alves <lucas_alvessp@icloud.com>
2013-09-21 00:18:00 -0300

    Adicionado grupos de usuários, permissões e usuários
    usuario: coordenacao
    senha: 123
    
    usuario: administrativo
    senha: 123
    
    usuario: academico
    senha: 123

--- database/sql/cjsmart.sql
+++ database/sql/cjsmart.sql
@@ -129,36 +129,6 @@ CREATE TABLE IF NOT EXISTS `endereco` (
   PRIMARY KEY (`id`)
 );
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `grupo_permissoes`
---
-
-DROP TABLE IF EXISTS `grupo_permissoes`;
-CREATE TABLE IF NOT EXISTS `grupo_permissoes` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `pagina` varchar(45) NOT NULL,
-  `visualizar` tinyint(1) NOT NULL DEFAULT '1',
-  `editar` tinyint(1) NOT NULL DEFAULT '1',
-  `apagar` tinyint(1) NOT NULL DEFAULT '1',
-  `usuario_grupos_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`)
-);
-
---
--- Extraindo dados da tabela `grupo_permissoes`
---
-
-INSERT INTO `grupo_permissoes` (`id`, `pagina`, `visualizar`, `editar`, `apagar`, `usuario_grupos_id`) VALUES
-(1, 'aluno', 1, 1, 1, 1),
-(2, 'curso', 1, 1, 1, 1),
-(3, 'educador', 1, 1, 1, 1),
-(4, 'home', 1, 1, 1, 1),
-(5, 'materia', 1, 1, 1, 1),
-(19, 'responsavel', 1, 1, 1, 1),
-(20, 'turma', 1, 1, 1, 1),
-(21, 'usuarios', 1, 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -234,50 +204,6 @@ CREATE  TABLE IF NOT EXISTS `cjsmart`.`aviso` (
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuarios`
---
-
-DROP TABLE IF EXISTS `usuarios`;
-CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(50) DEFAULT NULL,
-  `nome` varchar(50) DEFAULT NULL,
-  `ultimo_nome` varchar(45) DEFAULT NULL,
-  `usuario` varchar(45) DEFAULT NULL,
-  `senha` varchar(50) DEFAULT NULL,
-  `ativo` tinyint(1) NOT NULL DEFAULT '1',
-  `grupo_usuario_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`)
-);
-
---
--- Extraindo dados da tabela `usuarios`
---
-
-INSERT INTO `usuarios` (`id`, `email`, `nome`, `ultimo_nome`, `usuario`, `senha`, `ativo`, `grupo_usuario_id`) VALUES
-(2, 'admin@hotmail.com', 'Admin', 'admin', 'admin', '202cb962ac59075b964b07152d234b70', 1, 1);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuario_grupo`
---
-
-DROP TABLE IF EXISTS `usuario_grupo`;
-CREATE TABLE IF NOT EXISTS `usuario_grupo` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `nome` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
---
--- Extraindo dados da tabela `usuario_grupo`
---
-
-INSERT INTO `usuario_grupo` (`id`, `nome`) VALUES
-(1, 'master');
 
 -- --------------------------------------------------------
 
@@ -374,3 +300,143 @@ CREATE  TABLE IF NOT EXISTS `cjsmart`.`local` (
   `local` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`id`) )
 ENGINE = InnoDB;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `grupo_permissoes`
+--
+
+DROP TABLE IF EXISTS `grupo_permissoes`;
+CREATE TABLE IF NOT EXISTS `grupo_permissoes` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `pagina` varchar(45) NOT NULL,
+  `visualizar` tinyint(1) NOT NULL DEFAULT '1',
+  `editar` tinyint(1) NOT NULL DEFAULT '1',
+  `apagar` tinyint(1) NOT NULL DEFAULT '1',
+  `usuario_grupos_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=91 ;
+
+--
+-- Extraindo dados da tabela `grupo_permissoes`
+--
+
+INSERT INTO `grupo_permissoes` (`id`, `pagina`, `visualizar`, `editar`, `apagar`, `usuario_grupos_id`) VALUES
+(1, 'aluno', 1, 1, 1, 1),
+(2, 'curso', 1, 1, 1, 1),
+(3, 'educador', 1, 1, 1, 1),
+(4, 'home', 1, 1, 1, 1),
+(5, 'materia', 1, 1, 1, 1),
+(19, 'responsavel', 1, 1, 1, 1),
+(20, 'turma', 1, 1, 1, 1),
+(21, 'usuarios', 1, 1, 1, 1),
+(22, 'aula', 1, 1, 1, 1),
+(23, 'matricula', 1, 1, 1, 1),
+(24, 'diarioaula', 1, 1, 1, 1),
+(25, 'presenca', 1, 1, 1, 1),
+(26, 'FinalizarSemestre', 1, 1, 1, 1),
+(40, 'aluno', 0, 0, 0, 18),
+(41, 'curso', 1, 1, 1, 18),
+(42, 'educador', 1, 1, 1, 18),
+(43, 'home', 1, 1, 1, 18),
+(44, 'materia', 1, 1, 1, 18),
+(45, 'responsavel', 0, 0, 0, 18),
+(46, 'turma', 1, 1, 1, 18),
+(47, 'usuarios', 0, 0, 0, 18),
+(48, 'aula', 1, 1, 1, 18),
+(49, 'matricula', 0, 0, 0, 18),
+(50, 'diarioaula', 0, 0, 0, 18),
+(51, 'presenca', 0, 0, 0, 18),
+(52, 'FinalizarSemestre', 0, 0, 0, 18),
+(53, 'administrativo', 0, 0, 0, 18),
+(54, 'coordenacao', 1, 1, 1, 18),
+(55, 'academico', 0, 0, 0, 18),
+(56, 'coordenacao', 1, 1, 1, 1),
+(57, 'academico', 1, 1, 1, 1),
+(58, 'administrativo', 1, 1, 1, 1),
+(59, 'aluno', 1, 1, 1, 19),
+(60, 'curso', 0, 0, 0, 19),
+(61, 'educador', 0, 0, 0, 19),
+(62, 'home', 0, 0, 0, 19),
+(63, 'materia', 0, 0, 0, 19),
+(64, 'responsavel', 0, 0, 0, 19),
+(65, 'turma', 0, 0, 0, 19),
+(66, 'usuarios', 0, 0, 0, 19),
+(67, 'aula', 0, 0, 0, 19),
+(68, 'matricula', 1, 1, 1, 19),
+(69, 'diarioaula', 1, 1, 1, 19),
+(70, 'presenca', 1, 1, 1, 19),
+(71, 'FinalizarSemestre', 0, 0, 0, 19),
+(72, 'coordenacao', 0, 0, 0, 19),
+(73, 'academico', 0, 0, 0, 19),
+(74, 'administrativo', 1, 1, 1, 19),
+(75, 'aluno', 0, 0, 0, 20),
+(76, 'curso', 0, 0, 0, 20),
+(77, 'educador', 0, 0, 0, 20),
+(78, 'home', 0, 0, 0, 20),
+(79, 'materia', 0, 0, 0, 20),
+(80, 'responsavel', 0, 0, 0, 20),
+(81, 'turma', 0, 0, 0, 20),
+(82, 'usuarios', 0, 0, 0, 20),
+(83, 'aula', 0, 0, 0, 20),
+(84, 'matricula', 0, 0, 0, 20),
+(85, 'diarioaula', 0, 0, 0, 20),
+(86, 'presenca', 0, 0, 0, 20),
+(87, 'FinalizarSemestre', 1, 1, 1, 20),
+(88, 'coordenacao', 0, 0, 0, 20),
+(89, 'academico', 1, 1, 1, 20),
+(90, 'administrativo', 0, 0, 0, 20);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) DEFAULT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `ultimo_nome` varchar(45) DEFAULT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `senha` varchar(50) DEFAULT NULL,
+  `ativo` tinyint(1) NOT NULL DEFAULT '1',
+  `grupo_usuario_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Extraindo dados da tabela `usuarios`
+-- 
+
+INSERT INTO `usuarios` (`id`, `email`, `nome`, `ultimo_nome`, `usuario`, `senha`, `ativo`, `grupo_usuario_id`) VALUES
+(2, 'admin@hotmail.com', 'Admin', 'admin', 'admin', '202cb962ac59075b964b07152d234b70', 1, 1),
+(3, 'coordenacao@cjsmart.com', 'Coordenação', '', 'coordenacao', '202cb962ac59075b964b07152d234b70', 1, 18),
+(4, 'administrativo@cjsmart.com', 'Administrativo', '', 'administrativo', '202cb962ac59075b964b07152d234b70', 1, 19),
+(5, 'academico@cjsmart.com', 'Acadêmico', '', 'academico', '202cb962ac59075b964b07152d234b70', 1, 20);
+
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario_grupo`
+--
+
+DROP TABLE IF EXISTS `usuario_grupo`;
+CREATE TABLE IF NOT EXISTS `usuario_grupo` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
+
+--
+-- Extraindo dados da tabela `usuario_grupo`
+--
+
+INSERT INTO `usuario_grupo` (`id`, `nome`) VALUES
+(1, 'master'),
+(18, 'coordenacao'),
+(19, 'administrativo'),
+(20, 'academico');
