cc268473beef6319d46225e7640b00e16478d58c
Lucas Alves <lucas_alvessp@icloud.com>
2013-10-07 23:26:43 -0300

    Iteração Local em Add Atividade

--- public/app/webroot/js/Atividade.js
+++ public/app/webroot/js/Atividade.js
@@ -46,12 +46,60 @@
 			});
 		};
 
+		this.Local = {
+			Local: new Local,
+			realText: '',
+			alterInput: function(){
+				if($("#AtividadeLocalId").is(':visible')){
+					$("#AtividadeLocalId").fadeOut(300, function(){						
+						var input = document.createElement('input');
+						input.type = 'text';	
+						input.className = 'AtividadeLocalName';
+						$("#AtividadeLocalId").before(input);
+					});
+				}else{
+					$('.AtividadeLocalName').remove();
+					$("#AtividadeLocalId").fadeIn(300);
+				}
+			},
+			add: function(that){
+				this.realText = $(that).text();
+				$(that).removeClass('add-local btn-info').addClass('save-local btn-success').text('Salvar Local');
+				this.alterInput();
+			},
+			save: function(that){
+				$(that).removeClass('save-local btn-success').addClass('add-local btn-info').text(this.realText);
+				if($('.AtividadeLocalName').val().length){
+					this.Local.add({local: $('.AtividadeLocalName').val()}, function(local){
+						if(local.status){
+							var option = document.createElement('option');
+							option.value = local.Local.id;
+							option.innerHTML = local.Local.local;
+							$('#AtividadeLocalId').append(option);
+							$('#AtividadeLocalId option:last').attr('selected', true);
+						}else{
+							this.alterInput();
+						}
+					});
+				}
+				this.alterInput();
+			}
+		};
+
 		this.on = function(){
 			var self = this;
 
 			$('body').on('click', '.atividade-view a.editar', function(e){
 				e.preventDefault();
 				self.edit($(this).attr('href'));
+			});
+
+			$('body').on('click', '.add-local', function(e){
+				self.Local.add(this);
+			});
+
+			$('body').on('click', '.save-local', function(e){
+				self.Local.save(this);
 			});
 		};
 	};
