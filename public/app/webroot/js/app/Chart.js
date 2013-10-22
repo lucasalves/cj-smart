1cb8a5ba28a0dff21df098ad0d7a5fa6ad951856
Lucas Alves <lucas_alvessp@icloud.com>
2013-10-22 00:11:58 -0200

    App Chart "Helper"JS

---
+++ public/app/webroot/js/app/Chart.js
@@ -0,0 +1,96 @@
+var Charts = {
+                lines: function(selector, options){
+                        return new Highcharts.Chart({
+                                              credits: {
+                                              enabled: false
+                                            },
+                                    chart: {                        
+                                                renderTo: selector,                             
+                                        type: 'spline'
+                                    },
+                                    title: {
+                                            text: ''
+                                        },
+                                        xAxis: {                    
+                                          lineColor: 'transparent',
+                                          tickLength: 0,
+                                          categories: [],
+                                        },
+                                        yAxis: {
+                                          title: {
+                                            text: ''
+                                          },
+                                          gridLineWidth: 0,
+                                          lineWidth: 0,
+                                          minorGridLineWidth: 0,
+                                          lineColor: 'transparent',
+                                        },
+                                        legend: {
+                                            align: 'right',
+                                            x: -100,
+                                            verticalAlign: 'top',
+                                            y: 20,
+                                            floating: true,
+                                            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColorSolid) || 'white',
+                                            borderColor: '#CCC',
+                                            borderWidth: 1,
+                                            shadow: false
+                                        },
+                                        tooltip: options.tooltip || false,
+                                        plotOptions: {
+                                            column: {
+                                                borderWidth: 0,
+                                                stacking: 'normal',
+                                                dataLabels: {
+                                                    enabled: false,
+                                                    color: null
+                                                },
+                                                shadow: false
+                                            }
+                                        },
+                                        legend:{
+                                            enabled: false
+                                        },
+                                    series: options.series || []
+                                });
+                        },
+                        custom: function(selector, options){
+                        	console.log(options.series);
+                                return new Highcharts.Chart({
+                                    credits: {
+                                      enabled: false
+                                    },
+                            		chart: {                      
+                                        renderTo: selector,
+                                        backgroundColor: options.backgroundColor || '#FFFFFF'
+                            		},
+                                	title: {
+                              			text: ''
+                                	},
+                                	xAxis: { 
+                                		categories: options.categories || []
+                                	},
+                                	yAxis: {
+					                	title: {
+					                    	text: options.yAxis || ''
+					                	},
+					                	plotLines: [{
+					                    	value: 0,
+					                    	width: 1,
+					                    	color: '#808080'
+					                	}]
+					            	},
+                                	tooltip: options.tooltip || false,
+                               
+                                	legend: {
+							            itemDistance: 20
+							        },
+                            		exporting:{
+                              			enabled: true
+                            		},
+                            		series: options.series || []
+                                });
+                        }
+
+
+};
