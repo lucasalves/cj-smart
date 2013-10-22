74ff45d074d505a692f6b1082d307a5bd1ecf91b
Lucas Alves <lucas_alvessp@icloud.com>
2013-10-22 00:12:11 -0200

    Aluno Chart Notas

--- public/app/webroot/js/aluno.js
+++ public/app/webroot/js/aluno.js
@@ -14,4 +14,68 @@ var Aluno = function(){
 	this.edit = function(id, addListerEvent){
 		App.Request.get('aluno/edit/' + id, {}, $('.aluno'), addListerEvent);		
 	};
+
+	this.isPageView = function(){
+		return ($(".aluno").length && $(".aluno").data('page') === 'view');
+	};
+
+	this.graph = {
+		GRAPH:   {}
+	};
+
+	this.getStats = function(addListerEvent){
+		var id = location.href.split(/\//).pop();
+		App.Request.get('aluno/stats/' + id, {}, $('stats-aluno'), addListerEvent, 'json');
+	};
+
+	this.loadStats = function(){
+		var self = this;
+		this.getStats(function(data){
+			var series = [];
+			var i = 0;
+
+			for (i = 0; i < data.matriculas.length; i++) {
+				data.matriculas[i].type = 'spline';
+				series.push(data.matriculas[i]);
+			};
+
+			series.push({
+                type: 'pie',
+                name: 'Media por Materia',
+                data: data.materias,
+                center: [100, 80],
+                size: 100,
+                showInLegend: false,
+                dataLabels: {
+                    enabled: false
+                }
+	       	});
+			console.log(series);
+			self.graph.GRAPH = new Charts.custom('stats-aluno', {
+				series: series,
+				tooltip: {
+	                formatter: function() {
+	                    var s;
+	                    if (this.point.name) { // the pie chart
+	                        s = 'Media em '+
+	                            this.point.name +': '+ this.y;
+	                    } else {
+	                        s = 'Evolução por nota'+
+	                            ': '+ this.y;
+	                    }
+	                    return s;
+	                }
+	            }
+			});
+		});
+	};
 };
+
+
+$(document).ready(function(){
+	var aluno = new Aluno();
+
+	if(aluno.isPageView()){
+		aluno.loadStats();
+	}
+});
